ecd86291affcc6b7a0585e55b65b533c5fc20e61
MaxVzl <maxime.vozelle@gmail.com>
2026-02-02 18:43:04 +0100

    Add ESLint and Prettier configuration files, update package.json scripts, and add necessary dependencies

---
+++ eslint.config.ts
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    // plugins: { js },
+    // extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      eqeqeq: 'error',
+      'prettier/prettier': 'error',
+    },
+  },
+]);
